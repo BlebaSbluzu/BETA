2f9aa23252c00528dc88e2481735162ab33c2772
jakubdomurat <B00153270@mytudublin.ie>
2023-12-16 21:50:21 +0000

    updated

---
+++ Boarman/Assets/Boarman/Scripts/Coin.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin: MonoBehaviour
+{
+    // Start is called before the first frame update
+   public float turnSpeed = 90f;
+   private void OnTriggerEnter(Collider other)
+   {
+if(other.gameObject.GetComponent<Obstacle>() != null)
+{
+Destroy(gameObject);
+return;
+}
+
+if (other.gameObject.name != "Player"){
+    return;
+}
+Destroy(gameObject);
+   }
+   
+
+   
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+
+}
--- Boarman/Assets/Boarman/Scripts/GroundSpawner.cs
+++ Boarman/Assets/Boarman/Scripts/GroundSpawner.cs
@@ -4,21 +4,33 @@ using UnityEngine.UIElements;
 
 public class GroundSpawner : MonoBehaviour
 {
-    public GameObject groundTile;
-    Vector3 nextSpawnPoint;
 
-   public void SpawnTile()
+    public GameObject groundTile;
+public int numberOfTilesToSpawn = 10;
+
+  Vector3 nextSpawnPoint;
+    private void Start()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            SpawnTile();
+        }
+    }
+
+    public void SpawnTile()
     {
         GameObject temp = Instantiate(groundTile, nextSpawnPoint, Quaternion.identity);
         nextSpawnPoint = temp.transform.GetChild(1).transform.position;
-    }
 
-    private void Start()
-    {
-        for (int i = 0; i < 50; i++) {
-            SpawnTile();
-        }
-           
+        // Decrease the number of tiles to spawn
+        numberOfTilesToSpawn--;
+
+        // Check if no more tiles need to be spawned
+        if (numberOfTilesToSpawn == 0)
+        {
+            // Optional: You can disable further tile spawning or perform other actions here
+            enabled = false; // Disable the script
         }
     
+}
 }
